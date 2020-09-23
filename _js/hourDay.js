aac9d486bb0e2b5e6468353d93c031dcf3cb2006
stackOvercrow <gabriel-reis1999@hotmail.com>
2020-09-22 21:51:35 -0300

    Img paths fixed

--- _js/hourDay.js
+++ _js/hourDay.js
@@ -23,21 +23,21 @@ function loadHour(){
     function changeAppearance(hourDay){
         if(hourDay === 'Matutino'){
             siteParagraph.innerHTML += `${hour} horas.<br>Estamos no turno Matutino.<br>`        
-            img.setAttribute('src', '../_img/matutino.png')
+            img.setAttribute('src', '_img/matutino.png')
             welcomeText.innerHTML = 'Bom Dia!'
             window.document.body.style.backgroundColor = '#A5D7D8' 
             bodyText.style.boxShadow = '0px 0px 5px #000000' 
             bodyText.style.color = '#000000'
         }else if(hourDay === 'Vespertino'){
             siteParagraph.innerHTML += `${hour} horas.<br>Estamos no turno Vespertino.<br>`
-            img.setAttribute('src', '../_img/vespertino.png')
+            img.setAttribute('src', '_img/vespertino.png')
             welcomeText.innerHTML = 'Boa Tarde!'
             window.document.body.style.backgroundColor = '#D0956B' 
             bodyText.style.boxShadow = '0px 0px 5px #5C4513' 
             bodyText.style.color = '#5C4513'
         }else{
             siteParagraph.innerHTML += `${hour} horas.<br>Estamos no turno Noturno.<br>`
-            img.setAttribute('src', '../_img/noturno.png')
+            img.setAttribute('src', '_img/noturno.png')
             welcomeText.innerHTML = 'Boa Noite!'
             window.document.body.style.backgroundColor = '#000000'
             bodyText.style.boxShadow = '0px 0px 5px red' 
